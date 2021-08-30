1315196d460be4156a7c6e4946cfd1d4899205ac
Najwan Aribena <74854286+najwanaribena@users.noreply.github.com>
2021-08-30 08:05:53 +0700

    Add do while loop

--- calculator.cpp
+++ calculator.cpp
@@ -5,36 +5,45 @@ int main() {
     float num1,
           num2,
           result;
-    char opr;
 
-    cout << "Masukkan Angka: ";
-    cin >> num1;
+    char opr,
+         more;
 
-    cout << "Masukkan Operator(+, -, *, /): ";
-    cin >> opr;
+    do {
+        cout << "Masukkan Angka: ";
+        cin >> num1;
 
-    cout << "Masukkan Angka: ";
-    cin >> num2;
+        cout << "Masukkan Operator(+, -, *, /): ";
+        cin >> opr;
 
-    switch(opr) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            result = num1 / num2;
-            break;
-        default:
-            result = 0;
-            break;
+        cout << "Masukkan Angka: ";
+        cin >> num2;
+
+        switch(opr) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        cout << "\n";
+        cout << num1 << " " << opr << " " << num2 << " = " << result << endl;
+
+        cout << "\nhitung lagi(y/n)? ";
+        cin >> more;
+        cout << "\n";
     }
-    cout << "\n";
-    cout << num1 << " " << opr << " " << num2 << " = " << result << endl;
+    while(more == 'y');
 
     return 0;
 }
